bd58ae7a7f23b19ab8afbdda3107d6c9e353371c
WithoutAName <withoutaname@withoutaname.eu>
2026-03-09 11:11:18 +0100

    Configure qt style

--- common/home/jb/config/qt.nix
+++ common/home/jb/config/qt.nix
@@ -1,16 +1,58 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
+let
+  catppuccin-kde-modified = pkgs.catppuccin-kde.override {
+    flavour = [ "mocha" ];
+    accents = [ "sapphire" ];
+    winDecStyles = [ "modern" ];
+  };
+in
 {
   qt = {
     enable = true;
-    platformTheme = "qtct";
+    platformTheme.name = "qtct";
     style.name = "kvantum";
   };
 
   home = {
-    packages = with pkgs; [ kdePackages.qtstyleplugin-kvantum ];
+    packages = with pkgs; [
+      catppuccin-kde-modified
+      kdePackages.qtstyleplugin-kvantum
+    ];
     sessionVariables = {
       QT_QPA_PLATFORMTHEME = "qt5ct";
     };
+  };
+
+  xdg = {
+    configFile = (
+      let
+        kdeGlobals = {
+          UiSettings = {
+            ColorScheme = "CatppuccinMochaSapphire";
+          };
+        };
+        qtSettings = {
+          Appearance = {
+            custom_palette = true;
+            icon_theme = "Papirus-Dark";
+            standard_dialogs = "xdgdesktopportal";
+            style = "kvantum-dark";
+          };
+          Interface = {
+            activate_item_on_single_click = 2;
+          };
+        };
+      in
+      {
+        "kdeglobals".text = lib.generators.toINI { } kdeGlobals;
+        "qt5ct/qt5ct.conf".text = lib.generators.toINI { } qtSettings;
+        "qt6ct/qt6ct.conf".text = lib.generators.toINI { } qtSettings;
+      }
+    );
+
+    # Hacky fix since it is for some reason not linked correctly when installed
+    dataFile."color-schemes/CatppuccinMochaSapphire.colors".source =
+      "${catppuccin-kde-modified}/share/color-schemes/CatppuccinMochaSapphire.colors";
   };
 }
