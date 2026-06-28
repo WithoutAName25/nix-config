ed4e397465521ef86b91983c4a3f2a2feb26f025
WithoutAName <withoutaname@withoutaname.eu>
2026-06-28 19:13:25 +0200

    Install network utils

--- common/home/jb/programs/packages.nix
+++ common/home/jb/programs/packages.nix
@@ -2,15 +2,19 @@
 
 {
   home.packages = with pkgs; [
+    bind.dnsutils
     discord-ptb
     fd
     hunspell
     hunspellDicts.de_DE
     hunspellDicts.en_US
     kdePackages.ark
+    knot-dns
     libreoffice-fresh
+    libqalculate
     qimgv
     thunderbird
+    traceroute
     webcord
     yubikey-manager
     yubioath-flutter
