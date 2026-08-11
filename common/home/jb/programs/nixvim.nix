b00e5473596e40af89cf8b5012cdeb0f36fb3dd2
WithoutAName <withoutaname@withoutaname.eu>
2026-08-11 11:25:18 +0200

    Add gitcommit config to nixvim

--- common/home/jb/programs/nixvim.nix
+++ common/home/jb/programs/nixvim.nix
@@ -57,6 +57,22 @@
       signcolumn = "yes";
     };
 
+    autoCmd = [
+      {
+        event = [ "FileType" ];
+        pattern = [ "gitcommit" ];
+        callback.__raw = ''
+          function()
+            vim.opt_local.textwidth = 72
+            vim.opt_local.colorcolumn = "51,73"
+            vim.opt_local.spell = true
+            vim.opt_local.formatoptions:append("t")
+            vim.opt_local.formatoptions:append("n")
+          end
+        '';
+      }
+    ];
+
     colorschemes.catppuccin = {
       enable = true;
       settings = {
